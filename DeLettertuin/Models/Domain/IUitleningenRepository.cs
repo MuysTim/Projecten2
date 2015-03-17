7c427804efb25f9673f842476b3916a22c63f2e3
MuysTim <timmuys1994@hotmail.com>
2015-03-17 11:02:30 +0100

    a5

---
+++ DeLettertuin/Models/Domain/IUitleningenRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using DeLettertuin.Models.DAL;
+
+namespace DeLettertuin.Models.Domain
+{
+    public interface IUitleningenRepository
+    {
+        Uitlening FindBy(int itemId);
+        IQueryable<Uitlening> FindAll();
+        void Add(Uitlening uitlening);
+        void Delete(Uitlening uitlening);
+        void SaveChanges();
+    }
+}
