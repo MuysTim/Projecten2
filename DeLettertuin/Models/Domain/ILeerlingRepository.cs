32adccbaed869d118ef72c24842256678e77859b
MuysTim <timmuys1994@hotmail.com>
2015-03-26 12:27:43 +0100

    add repository

---
+++ DeLettertuin/Models/Domain/ILeerlingRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DeLettertuin.Models.Domain
+{
+    public interface ILeerlingRepository
+    {
+        Leerling FindBy(int itemId);
+        IQueryable<Leerling> FindAll();
+        void Add(Leerling leerling);
+        void Delete(Leerling leerling);
+        void SaveChanges();
+    }
+}
