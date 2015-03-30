c4ed3c90c7e2801928865273043942424eedb228
MuysTim <timmuys1994@hotmail.com>
2015-03-30 15:42:52 +0200

    repository

---
+++ DeLettertuin/Models/Domain/Lener.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace DeLettertuin
+{
+    public class Lener
+    {
+        public Lener(string naam, int id)
+        {
+            Naam = naam;
+            Id = id;
+        }
+
+        public ICollection<Uitlening> Uitleningen { get; set; }
+
+        public int Id { get; set; }
+
+        public string Naam { get; set; }
+
+        public void LeenUit()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
